99548803367982ac8cfe57709364b51f1bd6f94b
sanjaruzb <sanjar21021993@gmail.com>
2024-06-24 22:59:36 +0500

    sanjar

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         //$schedule->command('changehour employee')->dailyAt("01:00");
+        $schedule->command('tt-generate')->everyFiveMinutes();
     }
 
     /**
